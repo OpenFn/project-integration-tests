2fde7b5973fd27e24352d883f5ac71867ba80bae
Joe Clark <joe@openfn.org>
2025-10-21 16:36:59 +0100

    comment out test

--- test/runners/lightning.test.ts
+++ test/runners/lightning.test.ts
@@ -3,10 +3,10 @@ import * as runner from "../../src/runner/lightning";
 import { deepEquals } from "bun";
 import { expect } from "bun:test";
 
-const test = initTest(import.meta.filename);
+// const test = initTest(import.meta.filename);
 
-test("should run a mix command", async (ctx) => {
-  const result = await runner.test();
+// test.skip("should run a mix command", async (ctx) => {
+//   const result = await runner.test();
 
-  expect(result).toMatch("compiled with Erlang/OTP");
-});
+//   expect(result).toMatch("compiled with Erlang/OTP");
+// });
