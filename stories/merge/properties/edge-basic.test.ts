54c29e57db5e87eb9b5f1cee2b964f855bee194e
Joe Clark <joe@openfn.org>
2025-11-07 15:12:57 +0000

    update edge tests - but we can't do a lot here because projects doesn't convert labels or conditions

--- stories/merge/properties/edge-basic.test.ts
+++ stories/merge/properties/edge-basic.test.ts
@@ -5,39 +5,42 @@ const test = initTest(import.meta.filename);
 // These are tests on "basic" merges on edges, which I define
 // as anything without an id change
 
-// TODO need support from the generator for this
-test.skip("merge disabling an edge", async (ctx: Context) => {
+// TODO this passes right now  - but as commented in kit code
+// I think enabled state is an option and shouldn't sync!
+test("merge disabling an edge", async (ctx: Context) => {
   const main = `x-y`;
-  const staging = `x-(enabled=false)-y`;
-  const expected = `x-(enabled=false)-y`;
+  const staging = `x-(disabled=true)-y`;
+  const expected = `x-(disabled=true)-y`;
 
   await merge(ctx, main, staging, expected);
 });
 
-test.skip("merge enabling an edge", async (ctx: Context) => {
-  const main = `x-(enabled=false)-y`;
-  const staging = `x-(enabled=true)-y`;
-  const expected = `x-(enabled=true)-y`;
+test("merge enabling an edge", async (ctx: Context) => {
+  const main = `x-(disabled=true)-y`;
+  const staging = `x-(disabled=false)-y`;
+  const expected = `x-(disabled=false)-y`;
 
   await merge(ctx, main, staging, expected);
 });
 
+// Turns out I can't test any of the edge condition stuff thanks to https://github.com/OpenFn/kit/issues/1123
 test.skip("merge adding an edge condition", async (ctx: Context) => {
   const main = `x-y`;
-  const staging = `x-(condition=!state.errors)-y`;
-  const expected = `x-(condition=!state.errors)-y`;
+  const staging = `x-(condition="!state.errors")-y`;
+  const expected = `x-(condition="!state.errors")-y`;
 
   await merge(ctx, main, staging, expected);
 });
 
 test.skip("merge changing an edge condition", async (ctx: Context) => {
-  const main = `x-(condition=!state.errors)-y`;
-  const staging = `x-(condition=false)-y`;
-  const expected = `x-(condition=false)-y`;
+  const main = `x-(condition="!state.errors")-y`;
+  const staging = `x-(condition="false")-y`;
+  const expected = `x-(condition="false")-y`;
 
   await merge(ctx, main, staging, expected);
 });
 
+// This is also not tracked at the moment
 test.skip("merge adding an edge label", async (ctx: Context) => {
   const main = `x-y`;
   const staging = `x-(label=a)-y`;
